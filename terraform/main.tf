1439849441adc98648c5728887d305df4321e65d
Peter Connolly <pmconnolly80@gmail.com>
2026-07-13 15:44:44 -0400

    update dns for load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,8 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_sqs_queue" "thumbnail_complete" {
+  name                      = "thumbnail-complete"
+  message_retention_seconds = 86400  # 1 day
+}
